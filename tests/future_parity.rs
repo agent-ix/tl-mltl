8cc92537e236082dec7fe87b13fd43345d3f90a3
Peter Krenesky <kreneskyp@gmail.com>
2026-09-24 01:49:22 -0700

    test: update reviewed mlTL source census

--- tests/future_parity.rs
+++ tests/future_parity.rs
@@ -1153,7 +1153,7 @@ fn evaluator_has_no_derived_future_branch() {
         scanned += 1;
     }
     assert_eq!(
-        scanned, 26,
+        scanned, 29,
         "the reviewed subsystem source population changed"
     );
 }
